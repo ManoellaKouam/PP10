f9cf0f1246a192a7ee34f79b4f41fff5fc5a23f5
ManoellaKouam <michelle.kouam-wogaing@stud.thga>
2025-06-25 11:25:37 +0200

    Add solutions files for tasks

---
+++ solutions/json_main.c
@@ -0,0 +1,12 @@
+#include <jansson.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+json_t *root = json_pack("{s:i, s:s}", "id", 1, "name", "Alice");
+char *dump = json_dumps(root,0);
+printf("%s\n",dump);
+free (dump);
+json_decref(root);
+return 0;
+}
---
+++ solutions/util.c
@@ -0,0 +1,5 @@
+int clamp(int v, int lo, int hi) {
+    if (v < lo) return lo;
+    if (v > hi) return hi;
+    return v;
+}
